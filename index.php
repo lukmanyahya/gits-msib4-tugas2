2e9e00f9572417641ca1f09522cba70d6201c30c
lukmanyahya <105480771+lukmanyahya@users.noreply.github.com>
2023-02-26 18:25:57 +0700

    membuat file css, php, html

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> Saya </title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body background="1.jpg">
+<header class="header">
+    <marquee><font size="100" color="blue">LUKMAN YAHYA SAPUTRA</marquee>
+    </header>
+    
+<nav class="topnav">
+    <a href="https://www.facebook.com">Facebook</a>
+    <a href="https://www.youtube.com">Youtube</a>
+    <a href="https://www.google.com">Google</a>
+    <a href="https://www.bing.com">Bing</a>
+</nav>
+
+<div class="row">
+    <article class="leftcolumn">
+<div class="card">
+<img src="1.jpeg" width="640" height="360">
+        </article>
+        
+    <aside class="rightcolumn">
+<div class="card">
+<div class="fakeimg" style="height:100px;">Bingung</div>
+</div>
+
+            
+<footer class="footer"></footer>
+
+</body>
+</html>
+
